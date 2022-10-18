e6d35cd2cac56db491dde638700afcf114723e07
Luca Lischetti <luca@sirlisko.com>
2022-10-18 20:41:14 +0100

    feat: invite

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import "./App.scss";
 
 import Face from "./Face/Face";
 import Home from "./Home/Home";
-import Invite from "./Invite/Invite";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/invite" element={<Invite />}></Route>
           <Route path="*" element={<Home />}></Route>
         </Routes>
         <Face />
