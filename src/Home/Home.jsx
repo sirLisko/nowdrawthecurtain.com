9d153920dd9f520be67a8faa958f47a1c9e5bf4a
Luca Lischetti <luca@sirlisko.com>
2022-10-18 23:20:49 +0100

    feat: various optimisations

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,16 +4,18 @@ import Event from "../Event/Event";
 import Venue from "../Venue/Venue";
 
 function Home() {
-  const invite = window.location.search.split("?x=")?.[1];
+  const invite =
+    window.location.search.split("?invite=")?.[1] ||
+    window.location.search.split("?x=")?.[1];
+  let name;
+  try {
+    name = atob(invite);
+  } catch {}
   return (
     <div>
       <div className="App">
         <header className="App-header">
-          {invite && (
-            <h2>
-              <b>{atob(invite)}</b> <span>you are invited to</span>
-            </h2>
-          )}
+          {name && <h2>{name}, you are invited to</h2>}
           <h1>Now draw the curtain</h1>
           <p>
             Solo Exhibition by <a href="https://untepid.com">Ana Curbelo</a>
@@ -33,9 +35,17 @@ function Home() {
             </span>
             <span>The Book Club</span>
             <span>EC2A 4RH</span>
+            <span style={{ marginTop: "1rem" }}>
+              <a
+                href="https://www.eventbrite.co.uk/e/svp-presents-ana-curbelo-exhibition-opening-party-at-the-book-club-tickets-444025300627"
+                title="Get your spot"
+              >
+                RSVP here
+              </a>
+            </span>
           </div>
         </div>
-        <Footer />
+        <Footer hideLogos />
       </div>
       <Event />
       <Bio />
