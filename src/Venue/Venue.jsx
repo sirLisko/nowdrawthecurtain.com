1447c95252ddef02227773708ab4a378109133e9
Luca Lischetti <luca@sirlisko.com>
2022-10-27 20:18:02 +0100

    feat: added venue times

--- src/Venue/Venue.jsx
+++ src/Venue/Venue.jsx
@@ -5,6 +5,12 @@ const Venue = () => (
   <div className={styles.box}>
     <div className={styles.container}>
       <h3>The Venue</h3>
+      <div className={styles.times}>
+        <span>Weds: 5pm - 12am</span>
+        <span>Thurs: 5pm - 2am</span>
+        <span>Fridays: 4pm - 3am</span>
+        <span>Saturdays: 12pm - 3am</span>
+      </div>
       <iframe
         title="map"
         width="100%"
@@ -12,7 +18,7 @@ const Venue = () => (
         frameBorder="0"
         referrerPolicy="no-referrer-when-downgrade"
         src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GMAP_API}&q=Book+Club,London+UK`}
-      ></iframe>
+      />
     </div>
     <Footer />
   </div>
